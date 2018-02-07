5cfaf1784162005073948fbe160ac6e3209cc033
Jesse Pinho <jesse@jessepinho.com>
2018-02-08 00:48:12 +0100

    fix: move getStaticFile middleware after setPackageCookies

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ import { setPackageCookies } from './setPackageCookies'
 
 const app = express()
 app.use(cookieParser())
-app.use(getStaticFile)
 app.use(setPackageCookies)
+app.use(getStaticFile)
 
 // tslint:disable-next-line:no-console
 app.listen(3000, () => console.log('App listening on port 3000'))
