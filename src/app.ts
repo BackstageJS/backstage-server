975b3b89c44d0ab7c5605f74bb6d9d2678fbb316
Jesse Pinho <jesse@jessepinho.com>
2018-02-07 23:29:54 +0100

    feat: add cookie-parser

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
+import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 
 const app = express()
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+app.use(cookieParser())
 
 // tslint:disable-next-line:no-console
 app.listen(3000, () => console.log('App listening on port 3000'))
