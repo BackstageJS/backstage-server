20e6941754e62a6b469576cbf83bf9a1c0c4f3bd
Jesse Pinho <jesse@jessepinho.com>
2018-02-08 01:46:11 +0100

    feat: return an app rather than starting one

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ import * as express from 'express'
 import { getStaticFile } from './getStaticFile'
 import { setPackageFromQueryParams } from './setPackageFromQueryParams'
 
-const app = express()
-app.use(cookieParser())
-app.use(setPackageFromQueryParams)
-app.use(getStaticFile)
-
-// tslint:disable-next-line:no-console
-app.listen(3000, () => console.log('App listening on port 3000'))
+export const getApp = () => {
+  const app: express.Express = express()
+  app.use(cookieParser())
+  app.use(setPackageFromQueryParams)
+  app.use(getStaticFile)
+  return app
+}
