608003a03e4f400b03d5ac7e012e0ad9f3f30c38
Jesse Pinho <jesse@jessepinho.com>
2018-02-08 02:22:27 +0100

    refactor: allow a storage backend to be passed into getApp

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 
-import { getStaticFile } from './getStaticFile'
 import { setPackageFromQueryParams } from './setPackageFromQueryParams'
 
-export const getApp = () => {
+export type StorageBackend = express.RequestHandler
+
+export const getApp = (storageBackend: StorageBackend) => {
   const app: express.Express = express()
   app.use(cookieParser())
   app.use(setPackageFromQueryParams)
-  app.use(getStaticFile)
+  app.use(storageBackend)
   return app
 }
