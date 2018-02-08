da74396bff490ad2a35dea5fb6b9514d4e3548fb
Jesse Pinho <jesse@jessepinho.com>
2018-02-08 23:06:44 +0100

    refactor: convert fileSystem into a router

--- src/storage-backends/file-system/index.ts
+++ src/storage-backends/file-system/index.ts
@@ -1,10 +1,11 @@
-import { RequestHandler } from 'express'
+import * as express from 'express'
 
 import { StorageBackend } from '../../app'
 import { getFile } from './get-file'
 
-export const fileSystem: (rootDir: string) => StorageBackend = rootDir => (req, res, next) => {
-  if (req.method === 'GET') {
-    return getFile(rootDir)(req, res, next)
-  }
+export const fileSystem: (rootDir: string) => StorageBackend = rootDir => {
+  const router: express.Router = express.Router()
+  router.get('/', getFile(rootDir))
+
+  return router
 }
