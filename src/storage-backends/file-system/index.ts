1bec2b8f85dc35a3fe9b1b88609f7bf937d9cd9b
Jesse Pinho <jesse@jessepinho.com>
2018-02-10 01:00:13 +0100

    feat: upload the file to a /tmp directory

--- src/storage-backends/file-system/index.ts
+++ src/storage-backends/file-system/index.ts
@@ -6,7 +6,7 @@ import { getFile } from './get-file'
 import { postFile } from './post-file'
 
 export const fileSystem: (rootDir: string) => StorageBackend = rootDir => {
-  const upload = multer({ dest: rootDir })
+  const upload = multer({ dest: `${rootDir}/tmp` })
   const app: express.Express = express()
   app.get('/*', getFile(rootDir))
   app.post('/:app/:key', upload.single('package'), postFile(rootDir))
