feaab9e57038474e12ee50f853607cf97e3180ae
Jesse Pinho <jesse@jessepinho.com>
2018-02-18 16:24:26 +0100

    style: make postFile() a little more readable

--- src/storage-backends/file-system/post-file.ts
+++ src/storage-backends/file-system/post-file.ts
@@ -6,8 +6,11 @@ export const postFile: (rootDir: string) => RequestHandler = rootDir => (req, re
   const app = req.params.app.replace(/\W/g, '-')
   const key = req.params.key.replace(/\W/g, '-')
   const targetDirectory = `${rootDir}/${app}/${key}`
+
   rmdir(targetDirectory, () => null)
   mkdir(targetDirectory, () => null)
+
   tar.extract({ cwd: targetDirectory, file: req.file.path })
+
   res.status(201).end()
 }
