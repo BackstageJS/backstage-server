af0ad03675cd789c471c0edcf175b682550fd33d
Nathanael <nathanael.b.smith@gmail.com>
2019-07-14 10:39:27 -0500

    lowercase sanitized branch and app name to handle subdomain (#1)

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,1 +1,1 @@
-export const sanitizeName = (appOrKey: string): string => appOrKey.replace(/\W/g, '-')
+export const sanitizeName = (appOrKey: string): string => appOrKey.replace(/\W/g, '-').toLowerCase()
